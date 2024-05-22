148f48089d516e2fff4ca5fa2852fb0c9bf8977c
rnsp202 <165612437+rnsp202@users.noreply.github.com>
2024-05-23 01:18:44 +0900

    project

--- project/script/diet.js
+++ project/script/diet.js
@@ -2,3 +2,15 @@
 /*  -현재 키와 몸무게를 자유롭게 입력하고
     -버튼을 눌렀을 때 최종 목표 칸에 메세지 출력하기
     결과 예시) 적정체중은 ?kg이며 ?kg 초과되셨습니다.    */
+const height = document.querySelector('#height')
+console.log(height)
+const weight = document.querySelector('#weight')
+console.log(weight)
+const goals = document.querySelector('#goals')
+console.log(goals)
+const result = document.getElementById('result')
+result.addEventListener('click',function(){
+    let total = (Number(height.value) - 100) * 0.9
+    let over = total-Number(weight.value)
+    goals.value = `적정체중은 ${total}kg 이며 ${over}kg 초과되셨습니다.`
+})
